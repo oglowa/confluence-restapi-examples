47769c753a0845bfd781ace6dbf4870a1fee15b1
oglowa <>
2025-04-11 13:26:00 +0200

    glo03-666: initial commit

--- script/common/env-restapi.sh
+++ script/common/env-restapi.sh
@@ -6,16 +6,26 @@
 #   - Alternative: https://curl.se/windows/
 #
 # (c) 2024 Oliver Glowa (glo03) / Arvato Systems GmbH
-# 
+#
 # !!!! Works only with privat access token (PAT) !!!!
 
+if [ -f ~/.restapi/my.conf ]; then
+  source ~/.restapi/my.conf
+else
+  echo -e "No personal config found in ~/.restapi/my.conf!"
+  exit 11
+fi
+
+CONF_BASE_URL=${MY_BASE_URL}
+CONF_AUTH="Authorization: Bearer ${MY_PAT}"
+
+# Check
+if [ "${CONF_AUTH}" = "" ]; then
+	echo -e "No PAT (personal access token) in environment defined!"
+	exit 10
+fi
+
 TS=`date`
-
-CONF_AUTH="Authorization: Bearer ${CONF_PAT_TEST}"
-
-CONF_BASE_URL_TEST=https://nma-s-confluence-test.arvato-systems.de
-CONF_BASE_URL_INTE=https://nma-s-confluence-mig.arvato-systems.de
-CONF_BASE_URL=${CONF_BASE_URL_TEST}
 
 CONF_RESTAPI_URL=${CONF_BASE_URL}/rest/api/content
 DL_DIR=${TEMP}
@@ -23,14 +33,9 @@ DL_DIR=${TEMP}
 REQP_LIGHT='&expand=space'
 REQP_FULL='&expand=body.storage,version,space'
 
-RESP_SINGLE='.id + " " + .space.key + " " + .title'
-RESP_RESULTS='.results[]| .id + " " + .space.key + " " + .title'
-RESP_BODY_SINGLE='.id + " " + .space.key + " " + .title,.body.storage.value'
-RESP_BODY_RESULTS='.results[]| .id + " " + .space.key + " " + .title + "\n" + .body.storage.value'
+RESP_SINGLE='.id + " [" + .space.key + "] \"" + .title + "\""'
+RESP_RESULTS='.results[]| .id + " [" + .space.key + "] \"" + .title + "\""'
+RESP_BODY_SINGLE='.id + " [" + .space.key + "] \"" + .title + "\"",.body.storage.value'
+RESP_BODY_RESULTS='.results[]| .id + " [" + .space.key + "] \"" + .title + "\"\n" + .body.storage.value'
 
 
-# Check
-if [ "${CONF_AUTH}" = "" ]; then
-	echo -e "No PAT (personal access token) in environment defined!"
-	exit 10
-fi
